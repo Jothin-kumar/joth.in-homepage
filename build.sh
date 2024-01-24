a2f7206479caf838416425b17ac66571b87c3b7f
Jothin Kumar <contact@jothin.tech>
2024-01-25 01:07:05 +0530

    url shortener

--- build.sh
+++ build.sh
@@ -12,3 +12,20 @@ python3 build/build.py
 printf "build.py ✅\n\n"
 cp -r assets/images build-output/img
 printf "copy images ✅\n\n"
+
+
+if [ -d "url-shortener-joth.in" ]
+then
+    cd "url-shortener-joth.in"
+    git pull
+    cd ../
+else
+    git clone https://github.com/Jothin-kumar/url-shortener-joth.in.git
+fi
+printf "git (url-shortener) ✅\n\n"
+
+cd "url-shortener-joth.in"
+python3 export.py
+cd ../
+cp "url-shortener-joth.in/_redirects" "build-output/_redirects"
+printf "url shortener ✅\n\n"
